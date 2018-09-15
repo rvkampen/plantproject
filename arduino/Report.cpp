0274a9dffd38e39136257cef5961c3f14980f8a3
R van Kampen <rvkampen+github@gmail.com>
2018-09-15 02:13:24 +0200

    remove debug code

--- arduino/Report.cpp
+++ arduino/Report.cpp
@@ -3,7 +3,6 @@
 Report::Report(uint32_t unixtime, int sensorcount)
 	: jsonBuffer(JSON_ARRAY_SIZE(sensorcount) + JSON_OBJECT_SIZE(2) + sensorcount * JSON_OBJECT_SIZE(3))
 {
-	Serial.println(JSON_ARRAY_SIZE(sensorcount) + JSON_OBJECT_SIZE(2) + sensorcount * JSON_OBJECT_SIZE(3));
 	root= &jsonBuffer.createObject();
 	root->set("time",unixtime);
 	measurements = &root->createNestedArray("sensor");

f9c735316171d0d37c8b047d49033aae704ef5e0
R van Kampen <rvkampen+github@gmail.com>
2018-09-25 21:45:05 +0200

    website data - thinning when long ago

--- website/data.php
+++ website/data.php
@@ -42,6 +42,20 @@ SELECT sensor_id
 FROM `measurements`
 WHERE measurement_time_server > DATE_SUB(NOW(),INTERVAL ? HOUR) AND measurements.measurement_value > 1
 SQL2;
+
+if($timespan > 50)
+{
+	$query_data = <<<SQL2
+	SELECT sensor_id
+		 , measurement_type
+		 , DATE_FORMAT(MIN(measurement_time_server), '%Y-%m-%dT%TZ') as time
+		 , ROUND(AVG(measurement_value),2) as value
+	FROM `measurements`
+	WHERE measurement_time_server > DATE_SUB(NOW(),INTERVAL ? HOUR) AND measurements.measurement_value > 1
+	GROUP BY sensor_id, measurement_type, UNIX_TIMESTAMP(measurement_time_server) DIV 300
+SQL2;
+}
+
 $stmt_data = $mysqli->prepare($query_data);
 $stmt_data->bind_param("d", $timespan);
 $stmt_data->execute();
